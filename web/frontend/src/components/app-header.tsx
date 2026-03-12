d2139e3f5bc93f1da983a0d29bf8ef4212d0edb2
Dome C. <dome@tel.co.th>
2026-03-12 17:33:34 +0700

    feat: rebrand frontend to MClaw

--- web/frontend/src/components/app-header.tsx
+++ web/frontend/src/components/app-header.tsx
@@ -152,7 +152,7 @@ export function AppHeader() {
 
         {/* Docs Link */}
         <Button variant="ghost" size="icon" className="size-8" asChild>
-          <a href="https://docs.picoclaw.io" target="_blank" rel="noreferrer">
+          <a href="https://docs.mclaw.io" target="_blank" rel="noreferrer">
             <IconBook className="size-4.5" />
           </a>
         </Button>
